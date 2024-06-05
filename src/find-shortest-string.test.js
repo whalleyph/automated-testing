2ce6df1e9fcfb55377a8dfac7b7c3b3c62d7c264
2405-011-PW <2405-011-pw@U-LDNKOR4ROAZY.corp.amazonworkspaces.com>
2024-06-05 15:17:37 +0100

    add shortest string and test

---
+++ src/find-shortest-string.test.js
@@ -0,0 +1,12 @@
+const findShortestString = require("./find-shortest-string");
+
+test("findShortestString finds the shortest string in an array", () => {
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+});
+
+test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
+  expect(findShortestString(["brave", "dance"])).toBe("brave");
+  expect(findShortestString(["one", "two", "three", "four", "five", "six", "seven"])).toBe("one");
+  expect(findShortestString(["a", "aa", "a a", "bbb", "bb", "b"])).toBe("a");
+});
